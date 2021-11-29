c4ef9b74b25c14ac7d2787a4e1ce0c205e2af88a
Mateusz Świercz <mateusz.swiercz@student.put.poznan.pl>
2021-11-29 16:27:59 +0100

    usunalem 3 pliki

--- button.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QAbstractButton
-
-
-class Button(QPushButton):
-    def __init__(self, rgb, parent=None):
-        super(Button, self).__init__(parent)
-        self.setStyleSheet("background-color: rgb({}, {}, {}); color: rgb(0, 0, 0);".format(rgb[0], rgb[1], rgb[2]))
-
--- line_edit.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QLineEdit
-
-class Line_Edit(QLineEdit):
-    def __init__(self, rgb, parent=None):
-        super(Line_Edit, self).__init__(parent)
-        self.setStyleSheet("background-color: rgb({}, {}, {}); color: black;".format(rgb[0], rgb[1], rgb[2]))
