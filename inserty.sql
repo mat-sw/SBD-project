fbdc7859c61c53c18f2ce6c1b4a5e95293dac122
Mateusz Świercz <mateusz.swiercz@student.put.poznan.pl>
2022-01-09 14:02:12 +0100

    poprawione skrypty sql na postgresa

--- inserty.sql
+++ inserty.sql
@@ -1,43 +1,32 @@
 -- PRODUCENCI
-INSERT INTO PRODUCENT
-VALUES (1, 'Solaris');
-
-INSERT INTO PRODUCENT
-VALUES (2, 'Moderus');
+INSERT INTO PRODUCENCI VALUES (1, 'Solaris');
+INSERT INTO PRODUCENCI VALUES (2, 'Moderus');
 
 -- MIASTA
-INSERT INTO MIASTO
+INSERT INTO MIASTA (nazwa_miasta, status, liczba_mieszkancow, powierzchnia) 
 VALUES('Poznań', 'miasto', 540365, 261.8);
 
-
-INSERT INTO MIASTO
+INSERT INTO MIASTA (nazwa_miasta, status, liczba_mieszkancow, powierzchnia) 
 VALUES('Rokietnica', 'wieś', 7523, 6.8);
 
-
-INSERT INTO MIASTO
-VALUES('Swarzędz', 'wieś', 30739, 9);
+INSERT INTO MIASTA (nazwa_miasta, status, liczba_mieszkancow, powierzchnia) 
+VALUES('Swarzędz', 'miasto', 30739, 9);
 
 -- STREFY
-INSERT INTO STREFA
-VALUES('A');
-
-INSERT INTO STREFA
-VALUES('ABC');
-
-INSERT INTO STREFA
-VALUES('B');
-
-INSERT INTO STREFA
-VALUES('C');
+INSERT INTO STREFA VALUES('A');
+INSERT INTO STREFY VALUES('B');
+INSERT INTO STREFY VALUES('C');
+INSERT INTO STREFY VALUES('AB');
+INSERT INTO STREFY VALUES('ABC');
 
 --KIEROWCY
-INSERT INTO kierowca VALUES('82032509225', 'Damian', 'Paczkowski', 'm', 't', 'n', 3415, to_date('2022-01-01', 'YYYY-MM-DD'), 'zonaty');
-INSERT INTO kierowca VALUES('82051517654', 'Daniel', 'Jankowski', 'm', 'n', 't', 3556, to_date('2022-01-01', 'YYYY-MM-DD'), 'kawaler');
-INSERT INTO kierowca VALUES('76111853890', 'Anna', 'Maćkowiak', 'k', 't', 'n', 3367, to_date('2022-01-01', 'YYYY-MM-DD'), 'panna');
-INSERT INTO kierowca VALUES('89041709876', 'Krystyna', 'Warda', 'k', 'n', 't', 3405, to_date('2022-01-01', 'YYYY-MM-DD'), 'mezatka');
+INSERT INTO kierowcy VALUES('82032509225', 'Damian', 'Paczkowski', 'mezczyzna', 'tak', 'nie', 3415, to_date('2022-01-01', 'YYYY-MM-DD'), 'zonaty');
+INSERT INTO kierowcy VALUES('82051517654', 'Daniel', 'Jankowski', 'mezczyzna', 'nie', 'tak', 3556, to_date('2022-01-01', 'YYYY-MM-DD'), 'kawaler');
+INSERT INTO kierowcy VALUES('76111853890', 'Anna', 'Maćkowiak', 'kobieta', 'tak', 'nie', 3367, to_date('2022-01-01', 'YYYY-MM-DD'), 'panna');
+INSERT INTO kierowcy VALUES('89041709876', 'Krystyna', 'Warda', 'kobieta', 'nie', 'tak', 3405, to_date('2022-01-01', 'YYYY-MM-DD'), 'mezatka');
 
 --BILETOMATY
-insert into biletomat VALUES(nextval('biletomat_seq'), 't', 't');
-insert into biletomat VALUES(nextval('biletomat_seq'), 'n', 't');
-insert into biletomat VALUES(nextval('biletomat_seq'), 't', 'n');
-insert into biletomat VALUES(nextval('biletomat_seq'), 't', 't');
+insert into biletomaty VALUES(nextval('biletomat_seq'), 'tak', 'tak');
+insert into biletomaty VALUES(nextval('biletomat_seq'), 'nie', 'tak');
+insert into biletomaty VALUES(nextval('biletomat_seq'), 'tak', 'nie');
+insert into biletomaty VALUES(nextval('biletomat_seq'), 'tak', 'tak');
--- procedury.sql
+++ procedury.sql
@@ -7,50 +7,55 @@ CREATE SEQUENCE bilet_seq START 1;
 CREATE SEQUENCE biletomat_seq START 1;
 CREATE SEQUENCE model_seq START 1;
 
-CREATE OR REPLACE TRIGGER UzupelnijCene
-    BEFORE INSERT ON bilety
-    FOR EACH ROW
+-- FUNCTIONS
+CREATE OR REPLACE FUNCTION UzupelnijCeny()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS $$
 BEGIN
-    IF :NEW.czy_ulgowy LIKE 't' THEN
-        IF :NEW.czas_przejazdu LIKE '15' THEN
-            UPDATE bilety SET cena = 2;
-        ELSIF :NEW.czas_przejazdu LIKE '30' THEN
-            UPDATE bilety SET cena = 3;
-        ELSE
-            UPDATE bilety SET cena = 4;
-        END IF;
-    ELSE
-       IF :NEW.czas_przejazdu LIKE '15' THEN
-            UPDATE bilety SET cena = 4;
-        ELSIF :NEW.czas_przejazdu LIKE '30' THEN
-            UPDATE bilety SET cena = 6;
-        ELSE
-            UPDATE bilety SET cena = 8;
-        END IF;
-    END IF;
-    IF :NEW.strefa_typ_strefy LIKE 'B' THEN
-            UPDATE bilety SET cena = cena + 0.5;
-    ELSIF :NEW.strefa_typ_strefy LIKE 'C' THEN
-            UPDATE bilety SET cena = cena + 0.6;
-    ELSIF :NEW.strefa_typ_strefy LIKE 'AB' THEN
-            UPDATE bilety SET cena = cena + 0.7;
-    ELSIF :NEW.strefa_typ_strefy LIKE 'ABC' THEN
-            UPDATE bilety SET cena = cena + 1;
-    END IF;
-END;
+	IF NEW.czy_ulgowy LIKE 'tak' THEN
+         IF czas_przejazdu LIKE '15' THEN
+             UPDATE bilety SET cena = 2;
+         ELSIF czas_przejazdu LIKE '30' THEN
+             UPDATE bilety SET cena = 3;
+         ELSE
+             UPDATE bilety SET cena = 4;
+         END IF;
+	ELSE
+		IF czas_przejazdu LIKE '15' THEN
+			 UPDATE bilety SET cena = 4;
+		ELSIF czas_przejazdu LIKE '30' THEN
+			UPDATE bilety SET cena = 6;
+		ELSE
+			UPDATE bilety SET cena = 8;
+		END IF;
+	END IF;
+	IF strefa_typ_strefy LIKE 'B' THEN
+		 UPDATE bilety SET cena = cena + 0.5;
+	ELSIF strefa_typ_strefy LIKE 'C' THEN
+		 UPDATE bilety SET cena = cena + 0.6;
+	ELSIF strefa_typ_strefy LIKE 'AB' THEN
+		 UPDATE bilety SET cena = cena + 0.7;
+	ELSIF strefa_typ_strefy LIKE 'ABC' THEN
+		 UPDATE bilety SET cena = cena + 1;
+	END IF;
+	RETURN NEW;
+END; $$
 
-
-CREATE OR REPLACE FUNCTION GestoscZaludnienia
-    (pNazwa IN miasta.nazwa_miasta%TYPE)
-    RETURN FLOAT IS
-    vGestosc FLOAT;
-    vLiczbaMieszkancow INTEGER;
-    vPowierzchnia FLOAT;
+CREATE OR REPLACE FUNCTION GestoscZaludnienia()
+    RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS $$
 BEGIN
-    SELECT liczba_mieszkancow, powierzchnia
-    INTO (vLiczbaMieszkancow, vPowierzchnia)
-    FROM miasta
-    WHERE nazwa_miasta = pNazwa;
-    vGestosc = vLiczbaMieszkancow / vPowierzchnia;
-    RETURN vGestosc;
-END GestoscZaludnienia;
+	UPDATE miasta SET gestosc_zaludnienia = liczba_mieszkancow / powierzchnia;
+	RETURN NEW;
+END; $$
+
+-- TRIGGERS
+CREATE TRIGGER UzupelnijCene
+    AFTER INSERT ON bilety
+	EXECUTE PROCEDURE UzupelnijCeny();    
+
+CREATE TRIGGER ObliczGestosc
+	AFTER INSERT ON miasta
+	EXECUTE PROCEDURE GestoscZaludnienia();
